d335628075caf22a65da1dd793aabeada4a15c54
Murat <muratsahin1@yandex.com>
2018-12-28 15:11:38 +0300

    Deneme

--- index.js
+++ index.js
@@ -34,6 +34,8 @@ app.use('/public', express.static(path.join(__dirname, '/public')));
 var mountPath = process.env.PARSE_MOUNT || '/parse';
 app.use(mountPath, api);
 
+//DENEME
+
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
   res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
